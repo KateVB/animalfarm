87fe2fddfeabec6b19caf4e0ec09011757a31f5b
Katrien <Katrien_Van_Bemten@barry-callebaut.com>
2015-10-27 10:33:32 +0100

    On request - exercise - added pig

---
+++ src/com/company/Varken.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by BE023573 on 27/10/2015.
+ */
+public class Varken implements Farmable{
+
+    @Override
+    public String makeNoise() {
+        return "Oink Oink";
+    }
+
+    @Override
+    public void move() {
+        // WIGGEL WAGGEL
+    }
+    
+}
